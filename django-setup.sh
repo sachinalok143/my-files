3ed475ec98538f2e2d38749abe197592854f9f91
sachin kumar <sachinalok143@gmail.com>
2018-03-25 17:24:20 +0530

    Create django-setup.sh

---
+++ django-setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get install --upgrade python3
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo su
+pip3 install django==1.8
+pip3 install Pillow
+pip3 install --upgrade django-crispy-forms
+pip3 install django-registration
+exit
+sudo pip3 install django-angular
